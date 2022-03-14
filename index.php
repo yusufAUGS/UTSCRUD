c602cd789abc9c58cb98743820ac634e91edd0c1
yusufAUGS <101551770+yusufAUGS@users.noreply.github.com>
2022-03-15 00:03:26 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php
+
+require 'function.php';
+$mahasiswa = query("SELECT * FROM mahasiswa");
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Data Mahasiswa</title>
+
+    
+</head>
+
+    <link rel="stylesheet" href="style.css"/>
+<body background="gambar1.jpg">
+<div class="header">
+    <h1>Daftar Mahasiswa</h1>
+    <a class="link" href="tambah.php">Tambah Data</a>
+    
+</div>
+
+  
+    <br><br>
+    
+    <center><table style="margin-left:auto;margin-right:auto"  border="1" cellpadding="10" cellspacing="0" bgcolor="white">
+
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Photo</th>
+            <th>Nama</th>
+            <th>NPM</th>
+            <th>Jurusan</th>
+            <th>Email</th>
+        </tr>
+        <center>
+
+        <?php $i = 1; ?>
+        <?php foreach ($mahasiswa as $row) : ?>
+            <tr>
+
+                <td> <?= $i; ?> </td>
+
+                <td>
+                    <a href="ubah.php?id=<?php echo $row["id"]; ?>">Ubah</a> |
+                    <a href="hapus.php?id=<?php echo $row["id"]; ?>" onclick="return confirm('Yakin Ingin Menghapus Data');  ">Hapus</a>
+                </td>
+                <td><img src="img/<?php echo $row["gambar"] ?>" width="50"></td>
+                <td><?php echo $row["nama"] ?></td>
+                <td><?php echo $row["npm"] ?></td>
+                <td><?php echo $row["jurusan"] ?></td>
+                <td><?php echo $row["email"] ?></td>
+            </tr>
+            <?php $i++; ?>
+        <?php endforeach; ?>
+
+    </table>
+
+</body>
+
+</html>
